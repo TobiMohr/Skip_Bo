28617abbaf34b2069164dfcc39eb58d7580243d3
TobiMohr <t-mohr@online.de>
2021-04-08 17:30:36 +0200

    comment

--- src/main/scala/de/htwg/se/yourgame/YourGame.scala
+++ src/main/scala/de/htwg/se/yourgame/YourGame.scala
@@ -5,6 +5,7 @@ import de.htwg.se.yourgame.model.Player
 object YourGame {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
-    println("Hellllllllllllllllllllllllo, " + student.name)
+    println("Hello, " + student.name)
   }
+  // Test
 }

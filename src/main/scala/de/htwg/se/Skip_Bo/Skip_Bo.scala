0852704e25911b86ef6b3a1304b98920b63b710f
Rebecca Schmidt <rebecca.sarah.schmidt@gmail.com>
2021-04-08 18:03:52 +0200

    Moin

--- src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
@@ -5,7 +5,7 @@ import de.htwg.se.Skip_Bo.model.Player
 object Skip_Bo {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
-    println("Hello, " + student.name)
+    println("Moin, " + student.name)
   }
   // Test
 }

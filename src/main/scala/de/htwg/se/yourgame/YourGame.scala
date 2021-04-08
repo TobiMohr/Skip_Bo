a8296004b565644243be01abf12910f9ed6ce4b9
TobiMohr <t-mohr@online.de>
2021-04-08 17:24:28 +0200

    changed String

--- src/main/scala/de/htwg/se/yourgame/YourGame.scala
+++ src/main/scala/de/htwg/se/yourgame/YourGame.scala
@@ -5,6 +5,6 @@ import de.htwg.se.yourgame.model.Player
 object YourGame {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
-    println("Hello, " + student.name)
+    println("Hellllllllllllllllllo, " + student.name)
   }
 }

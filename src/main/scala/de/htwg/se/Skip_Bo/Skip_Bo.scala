67fcec56bf2840741eeddd73f1134754bd9c12be
TobiMohr <t-mohr@online.de>
2021-04-09 10:06:44 +0200

    kleine Änderung test

--- src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
@@ -6,6 +6,16 @@ object Skip_Bo {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Moin, " + student.name)
+    println("Commit test")
   }
   // Test
+
+  case class Card(Colour: String, Number: Int) {
+    override def toString: String = Number + "(" + Colour + ")"
+  }
+
+
+
+
+
 }

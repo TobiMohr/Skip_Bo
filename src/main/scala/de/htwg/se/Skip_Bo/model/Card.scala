c0f44f5c0dafa98ab1081dab5814aa06eb2e2083
TobiMohr <t-mohr@online.de>
2021-04-15 18:02:02 +0200

    Zugehörige Scala Klassen in model

---
+++ src/main/scala/de/htwg/se/Skip_Bo/model/Card.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.Skip_Bo.model
+
+class Card {
+
+}
--- src/test/scala/de/htwg/se/Skip_Bo/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/Skip_Bo/model/CardSpec.scala
@@ -1,7 +1,20 @@
 package de.htwg.se.Skip_Bo.model
 
-import org.scalatest.{Matchers, WordSpec}
+
+import org.scalatest._
 
 class CardSpec extends WordSpec with Matchers {
 
+    "A Card" when{
+      "new"should{
+        val card = Card(String, int)
+        "have value colour" in{
+          card.1 should be(Colour)
+        }
+        "have value int" in {
+          card.2 should be > 0
+          card.2 should be <= 12
+        }
+      }
+    }
 }

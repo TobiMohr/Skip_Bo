33f8f351bb0827f3879aecd1e5228ae289c8161f
TobiMohr <t-mohr@online.de>
2021-04-15 20:07:39 +0200

    CardSpec kann auf toString testen und auf String bzw Int ( gehen auch noch andere Strings :C)

--- src/main/scala/de/htwg/se/Skip_Bo/model/Card.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/model/Card.scala
@@ -3,7 +3,7 @@ package de.htwg.se.Skip_Bo.model
 
 case class Card(colour: String, rank: Int){
 
-  val card = Card("red", 1)
+  override def toString:String = "a " + colour + " " + rank
 
 
 
--- src/test/scala/de/htwg/se/Skip_Bo/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/Skip_Bo/model/CardSpec.scala
@@ -5,25 +5,29 @@ import org.scalatest._
 
 class CardSpec extends WordSpec with Matchers {
 
-    "A Card" when{
-
-      "should consist of a rank and a colour"should{
-        val card= Card("red", 1)
-        "be red" in{
-          card.colour should be("red")
-        }
-        "be green" in {
-          card.colour should be("green")
-        }
-        "be blue" in{
-          card.colour should be("blue")
-        }
-        "greater 0" in {
-          card.rank should be >= 1
-        }
-        "lesser 13" in{
-          card.rank should be <= 12
-        }
+  "A Card" when{
+    "new" should{
+      val card1 = new Card("red", 1)
+      val card2 = new Card("green", 6)
+      val card3 = new Card("blue", 12)
+      "have a colour" in{
+        card1.colour should be("red")
+        card2.colour should be("green")
+        card3.colour should be("blue")
+      }
+      "have a rank" in{
+        card1.rank should be(1)
+        card2.rank should be(6)
+        card3.rank should be(12)
+      }
+      "have a nice String representation" in{
+        card1.toString should be("a red 1")
+        card2.toString should be("a green 6")
+        card3.toString should be("a blue 12")
       }
     }
+  }
 }
+
+
+

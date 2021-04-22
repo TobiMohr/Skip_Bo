836cef7f789976535f85db04ecc7140ad89c65e1
Rebecca Schmidt <rebecca.sarah.schmidt@gmail.com>
2021-04-22 20:14:06 +0200

    Tui iwie bissle

--- src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
@@ -1,12 +1,13 @@
 package de.htwg.se.Skip_Bo
 
-import de.htwg.se.Skip_Bo.model.Player
+import de.htwg.se.Skip_Bo.model.{Player, TUI}
 
 object Skip_Bo {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     println(" Wilkommen zum besten Spiel")
     val start = "Hallöle " + anmeld(args)
     println(start)
+    new TUI();
   }
 
   def anmeld(spieler:Array[String]):String ={

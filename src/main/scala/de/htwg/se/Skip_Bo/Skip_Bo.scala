6d51242ce191f5437506dc8c2b680c0e42286a3f
TobiMohr <t-mohr@online.de>
2021-04-22 21:09:34 +0200

    bisschen an der TUI gespielt

--- src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/Skip_Bo.scala
@@ -5,13 +5,21 @@ import de.htwg.se.Skip_Bo.model.{Player, TUI}
 object Skip_Bo {
   def main(args: Array[String]): Unit = {
     println(" Wilkommen zum besten Spiel")
-    val start = "Hallöle " + anmeld(args)
-    println(start)
+    val spieler1 = anmeld1(args)
+    val spieler2 = anmeld2(args)
+
+    println("Hallöle" + spieler1)
+    println("Hallöle" + spieler2)
+    println( spieler1 + " ist am zug")
     new TUI();
   }
 
-  def anmeld(spieler:Array[String]):String ={
-    readLine("Bitte Name eingeben: ")
+  def anmeld1(spieler:Array[String]):String ={
+    readLine("Spieler1 - Bitte Name eingeben: ")
+  }
+
+  def anmeld2(spieler:Array[String]):String ={
+    readLine("Spieler2 - Bitte Name eingeben: ")
   }
 
 
--- src/main/scala/de/htwg/se/Skip_Bo/model/TUI.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/model/TUI.scala
@@ -1,18 +1,27 @@
 package de.htwg.se.Skip_Bo.model
 
+import de.htwg.se.Skip_Bo.Skip_Bo
+
 class TUI {
   while(true) {
     val scanner = new java.util.Scanner(System.in)
     val line = scanner.nextLine
     line match {
-      case "p1" => println("lege Karte auf 1. Spieler Stack")
-      case "p2" => println("lege Karte auf 2. Spieler Stack")
-      case "p3" => println("lege Karte auf 3. Spieler Stack")
-      case "p4" => println("lege Karte auf 4. Spieler Stack")
-      case "m1" => println("lege Karte auf 1. Mittel Stack")
-      case "m2" => println("lege Karte auf 2. Mittel Stack")
-      case "m3" => println("lege Karte auf 3. Mittel Stack")
-      case "m4" => println("lege Karte auf 4. Mittel Stack")
+      case "p1" => println("legt Karte auf 1. Spieler Stack")
+      case "p2" => println("legt Karte auf 2. Spieler Stack")
+      case "p3" => println("legt Karte auf 3. Spieler Stack")
+      case "p4" => println("legt Karte auf 4. Spieler Stack")
+      case "m1" => println("legt Karte auf 1. Mittel Stack")
+      case "m2" => println("legt Karte auf 2. Mittel Stack")
+      case "m3" => println("legt Karte auf 3. Mittel Stack")
+      case "m4" => println("legt Karte auf 4. Mittel Stack")
+      case "end" => Beenden
     }
+
+    def Beenden: Unit = {
+      println("Der Zug ist beendet")
+      println("Nächster Spieler ist am Zug")
+    }
+
   }
 }

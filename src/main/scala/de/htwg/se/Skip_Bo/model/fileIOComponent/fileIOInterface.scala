3c39975960aca9f8c0445bb3ff5b81262f87ad52
Tobias <t-mohr@online.de>
2021-07-01 10:42:24 +0200

    Xml leicht verschönert

--- src/main/scala/de/htwg/se/Skip_Bo/model/fileIOComponent/fileIOInterface.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/model/fileIOComponent/fileIOInterface.scala
@@ -1,8 +1,29 @@
 package de.htwg.se.Skip_Bo.model.fileIOComponent
 
+import de.htwg.se.Skip_Bo.model.CardComponent.Card
+import de.htwg.se.Skip_Bo.model.CardComponent.Value.Values
 import de.htwg.se.Skip_Bo.model.GameComponent.GameInterface
 
 trait fileIOInterface {
   def load: GameInterface
   def save(game: GameInterface): Unit
+  def stringToValue(s: String): Values
+
+  def stack1: List[Card]
+  def stack2: List[Card]
+  def stack3: List[Card]
+  def stack4: List[Card]
+  def cp1: List[Card]
+  def cp2: List[Card]
+  def hs1p1: List[Card]
+  def hs2p1: List[Card]
+  def hs3p1: List[Card]
+  def hs4p1: List[Card]
+  def hs1p2: List[Card]
+  def hs2p2: List[Card]
+  def hs3p2: List[Card]
+  def hs4p2: List[Card]
+  def sp1: List[Card]
+  def sp2: List[Card]
+  def cover: List[Card]
 }
--- src/main/scala/de/htwg/se/Skip_Bo/model/fileIOComponent/fileIOXmlImpl/fileIO.scala
+++ src/main/scala/de/htwg/se/Skip_Bo/model/fileIOComponent/fileIOXmlImpl/fileIO.scala
@@ -12,210 +12,185 @@ import scala.xml.{Elem, NodeSeq, PrettyPrinter}
 
 class fileIO extends fileIOInterface {
 
+  // für ablagestapel
+  var stack1: List[Card] = List.empty
+  var stack2: List[Card] = List.empty
+  var stack3: List[Card] = List.empty
+  var stack4: List[Card] = List.empty
+
+  var cp1: List[Card] = List.empty
+  var cp2: List[Card] = List.empty
+
+  var hs1p1: List[Card] = List.empty
+  var hs2p1: List[Card] = List.empty
+  var hs3p1: List[Card] = List.empty
+  var hs4p1: List[Card] = List.empty
+
+  var hs1p2: List[Card] = List.empty
+  var hs2p2: List[Card] = List.empty
+  var hs3p2: List[Card] = List.empty
+  var hs4p2: List[Card] = List.empty
+
+  var sp1: List[Card] = List.empty
+  var sp2: List[Card] = List.empty
+
+  var cover: List[Card] = List.empty
+
   override def load: GameInterface = {
     var game: GameInterface = Game(List(List.empty, List.empty, List.empty, List.empty), List.empty, List.empty)
     val file = scala.xml.XML.loadFile("game.xml")
-
-    // für ablagestapel
-    var stacks:List[Card] = List.empty
-    var stacks2:List[Card] = List.empty
-    var stacks3:List[Card] = List.empty
-    var stacks4:List[Card] = List.empty
-
-    var hand1:List[Card] = List.empty
-    var hand2:List[Card] = List.empty
-
-    var hstack1:List[Card] = List.empty
-    var hstack2:List[Card] = List.empty
-    var hstack3:List[Card] = List.empty
-    var hstack4:List[Card] = List.empty
-
-    var hstack1p2:List[Card] = List.empty
-    var hstack2p2:List[Card] = List.empty
-    var hstack3p2:List[Card] = List.empty
-    var hstack4p2:List[Card] = List.empty
-
-    var ps1:List[Card] = List.empty
-    var ps2:List[Card] = List.empty
-
-    var covered: List[Card] = List.empty
-
-
-    val test1 = (file \\ "stack1")
-    val test2 = (test1 \\ "cC")
-    val s1Cards = (test2 \\ "card")
-    for (card <- s1Cards) {
-      val cardtmp: Card = Card(stringToValue(card.text))
-      stacks = cardtmp +: stacks
-    }
-    stacks = stacks.reverse
-
-    val test21 = (file \\ "stack2")
-    val test22 = (test21 \\ "cC")
-    val s2Cards = (test22 \\ "card")
-    for (card <- s2Cards) {
-      val cardtmp: Card = Card(stringToValue(card.text))
-      stacks2 = cardtmp +: stacks2
-    }
-    stacks2 = stacks2.reverse
-
-    val test31 = (file \\ "stack3")
-    val test32 = (test31 \\ "cC")
-    val s3Cards = (test32 \\ "card")
-    for (card <- s3Cards) {
-      val cardtmp: Card = Card(stringToValue(card.text))
-      stacks3 = cardtmp +: stacks3
-    }
-    stacks3 = stacks3.reverse
-
-    val test41 = (file \\ "stack4")
-    val test42 = (test41 \\ "cC")
-    val s4Cards = (test42 \\ "card")
-    for (card <- s4Cards) {
-      val cardtmp: Card = Card(stringToValue(card.text))
-      stacks4 = cardtmp +: stacks4
-    }
-    stacks4 = stacks4.reverse
-
-
-    //Spieler section
-
-    //player1
-    val p1 = (file \\ "p1")
-    //name
-    val p1n: String = (p1 \\ "name").text
-    //hand
-    val handNodes = (p1 \\ "cards")
-    val hand = (handNodes \\"cC")
-    val handCard = (hand \\ "card")
-    for(card <- handCard){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hand1 = cardtmp +: hand1
-    }
-    //hepstacks
-    val hstacks = (p1 \\ "helpstacks")
-    val hstack1N = (hstacks \\"hstack1")
-    val hstack1N2 = (hstack1N \\"cC")
-    val hsCards1 = (hstack1N2 \\"card")
-    for (card <- hsCards1){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack1 = cardtmp +: hstack1
-    }
-
-    val hstack2N = (hstacks \\"hstack2")
-    val hstack2N2 = (hstack2N \\"cC")
-    val hsCards2 = (hstack2N2 \\"card")
-    for (card <- hsCards2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack2 = cardtmp +: hstack2
-    }
-
-    val hstack3N = (hstacks \\"hstack3")
-    val hstack3N2 = (hstack3N \\"cC")
-    val hsCards3 = (hstack3N2 \\"card")
-    for (card <- hsCards3){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack3 = cardtmp +: hstack3
-    }
-
-    val hstack4N = (hstacks \\"hstack4")
-    val hstack4N2 = (hstack4N \\"cC")
-    val hsCards4 = (hstack4N2 \\"card")
-    for (card <- hsCards4){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack4 = cardtmp +: hstack4
-    }
-
-    //pstack
-    val pstack = (p1 \\"stack")
-    val pstackN2 = (pstack \\"cC")
-    val psCards = (pstackN2 \\ "card")
-    for(card <- psCards){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      ps1 = cardtmp +: ps1
-    }
-
-    hand1 = hand1.reverse
-    hstack1 = hstack1.reverse
-    hstack2 = hstack2.reverse
-    hstack3 = hstack3.reverse
-    hstack4 = hstack4.reverse
-    ps1 = ps1.reverse
-    val player1 = Player(p1n, hand1, List(hstack1, hstack2, hstack3, hstack4), ps1)
-
-    val p2 = (file \\ "p2")
-    val p2n: String = (p2 \\ "name").text
-
-    val handNodesp2 = (p2 \\ "cards")
-    val handp2 = (handNodesp2 \\"cC")
-    val handCardp2 = (handp2 \\ "card")
-    for(card <- handCardp2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hand2 = cardtmp +: hand2
-    }
-    //hepstacks
-    val hstacksp2 = (p2 \\ "helpstacks")
-    val hstack1Np2 = (hstacksp2 \\"hstack1")
-    val hstack1N2p2 = (hstack1Np2 \\"cC")
-    val hsCards1p2 = (hstack1N2p2 \\"card")
-    for (card <- hsCards1p2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack1p2 = cardtmp +: hstack1p2
-    }
-
-    val hstack2Np2 = (hstacksp2 \\"hstack2")
-    val hstack2N2p2 = (hstack2Np2 \\"cC")
-    val hsCards2p2 = (hstack2N2p2 \\"card")
-    for (card <- hsCards2p2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack2p2 = cardtmp +: hstack2p2
-    }
-
-    val hstack3Np2 = (hstacksp2 \\"hstack3")
-    val hstack3N2p2 = (hstack3Np2 \\"cC")
-    val hsCards3p2 = (hstack3N2p2 \\"card")
-    for (card <- hsCards3p2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack3p2 = cardtmp +: hstack3p2
-    }
-
-    val hstack4Np2 = (hstacksp2 \\"hstack4")
-    val hstack4N2p2 = (hstack4Np2 \\"cC")
-    val hsCards4p2 = (hstack4N2p2 \\"card")
-    for (card <- hsCards4p2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      hstack4p2 = cardtmp +: hstack4p2
-    }
-
-    //pstack
-    val pstack2 = (p2 \\"stack")
-    val pstack2N2 = (pstack2 \\"cC")
-    val psCards2 = (pstack2N2 \\ "card")
-    for(card <- psCards2){
-      val cardtmp: Card = Card(stringToValue(card.text))
-      ps2 = cardtmp +: ps2
-    }
-    hand2 = hand2.reverse
-    hstack1p2 = hstack1p2.reverse
-    hstack2p2 = hstack2p2.reverse
-    hstack3p2 = hstack3p2.reverse
-    hstack4p2 = hstack4p2.reverse
-    ps2 = ps2.reverse
-    val player2 = Player(p2n, hand2, List(hstack1p2, hstack2p2, hstack3p2, hstack4p2), ps2)
-
-    // cardsCovered
-
-    val cc = (file \\ "cover")
-    val cc2 = (cc \\ "csC")
-    val cstackN2 = (cc2 \\ "card")
+    loadStacks(file)
+    val player1 = loadPlayer1(file)
+    val player2 = loadPlayer2(file)
+    val cstackN2 = getSeqFromFile(file, "cover", "csC")
     for(card <- cstackN2){
       val cardtmp:Card = Card(stringToValue(card.text))
-      covered = cardtmp +: covered
+      cover = cardtmp +: cover
     }
-    covered = covered.reverse
-    game = Game(List(stacks, stacks2, stacks3, stacks4), List(player1, player2), covered)
+    cover = cover.reverse
+
+    game = Game(List(stack1, stack2, stack3, stack4), List(player1, player2), cover)
     game
 
+  }
+
+    def loadStacks(file: Elem): Unit = {
+
+      val s1Cards = getSeqFromFile(file, "stack1", "cC")
+      for (card <- s1Cards) {
+        val cardtmp: Card = Card(stringToValue(card.text))
+        stack1 = cardtmp +: stack1
+      }
+      stack1 = stack1.reverse
+
+      val s2Cards = getSeqFromFile(file, "stack2", "cC")
+      for (card <- s2Cards) {
+        val cardtmp: Card = Card(stringToValue(card.text))
+        stack2 = cardtmp +: stack2
+      }
+      stack2 = stack2.reverse
+
+      val s3Cards = getSeqFromFile(file, "stack3", "cC")
+      for (card <- s3Cards) {
+        val cardtmp: Card = Card(stringToValue(card.text))
+        stack3 = cardtmp +: stack3
+      }
+      stack3 = stack3.reverse
+
+      val s4Cards = getSeqFromFile(file, "stack4", "cC")
+      for (card <- s4Cards) {
+        val cardtmp: Card = Card(stringToValue(card.text))
+        stack4 = cardtmp +: stack4
+      }
+      stack4 = stack4.reverse
+    }
+
+  def loadPlayer1(file: Elem): Player = {
+    val p1 = (file \\ "p1")
+
+    val p1n: String = (p1 \\ "name").text
+    val handCard = getSeqFromSeq(p1, "cards", "cC")
+    for(card <- handCard){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      cp1 = cardtmp +: cp1
+    }
+    val hstacks = (p1 \\ "helpstacks")
+
+    val hsCards1 = getSeqFromSeq(hstacks, "hstack1", "cC")
+    for (card <- hsCards1){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs1p1 = cardtmp +: hs1p1
+    }
+
+    val hsCards2 = getSeqFromSeq(hstacks, "hstack2", "cC")
+    for (card <- hsCards2){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs2p1 = cardtmp +: hs2p1
+    }
+
+    val hsCards3 = getSeqFromSeq(hstacks, "hstack3", "cC")
+    for (card <- hsCards3){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs3p1 = cardtmp +: hs3p1
+    }
+
+    val hsCards4 = getSeqFromSeq(hstacks, "hstack4", "cC")
+    for (card <- hsCards4){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs4p1 = cardtmp +: hs4p1
+    }
+
+    val psCards = getSeqFromSeq(p1, "stack", "cC")
+    for(card <- psCards){
+      val cardtmp: Card = Card(stringToValue(card.text))
+      sp1 = cardtmp +: sp1
+    }
+    cp1 = cp1.reverse
+    hs1p1 = hs1p1.reverse
+    hs2p1 = hs2p1.reverse
+    hs3p1 = hs3p1.reverse
+    hs4p1 = hs4p1.reverse
+    sp1 = sp1.reverse
+    val player1 = Player(p1n, cp1, List(hs1p1, hs2p1, hs3p1, hs4p1), sp1)
+    player1
+  }
+
+  def loadPlayer2(file: Elem): Player = {
+    val p2 = (file \\ "p2")
+    val p2name: String = (p2 \\ "name").text
+    val handCards = getSeqFromSeq(p2, "cards", "cC")
+    for (card <- handCards) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      cp2 = cardtmp +: cp2
+    }
+    val hstacksp2 = (p2 \\ "helpstacks")
+    val hsCards1p2 = getSeqFromSeq(hstacksp2, "hstack1", "cC")
+    for (card <- hsCards1p2) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs1p2 = cardtmp +: hs1p2
+    }
+    val hsCards2p2 = getSeqFromSeq(hstacksp2, "hstack2", "cC")
+    for (card <- hsCards2p2) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs2p2 = cardtmp +: hs2p2
+    }
+    val hsCards3p2 = getSeqFromSeq(hstacksp2, "hstack3", "cC")
+    for (card <- hsCards3p2) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs3p2 = cardtmp +: hs3p2
+    }
+    val hsCards4p2 = getSeqFromSeq(hstacksp2, "hstack4", "cC")
+    for (card <- hsCards4p2) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      hs4p2 = cardtmp +: hs4p2
+    }
+    val psCards2 = getSeqFromSeq(p2, "stack", "cC")
+    for (card <- psCards2) {
+      val cardtmp: Card = Card(stringToValue(card.text))
+      sp2 = cardtmp +: sp2
+    }
+    cp2 = cp2.reverse
+    hs1p2 = hs1p2.reverse
+    hs2p2 = hs2p2.reverse
+    hs3p2 = hs3p2.reverse
+    hs4p2 = hs4p2.reverse
+    sp2 = sp2.reverse
+    val player2 = Player(p2name, cp2, List(hs1p2, hs2p2, hs3p2, hs4p2), sp2)
+    player2
+  }
+
+  def getSeqFromFile(file: Elem, s:String, s2: String): NodeSeq = {
+    val first = (file \\ s)
+    val second = (first \\ s2)
+    val end = (second \\ "card")
+    end
+  }
+
+  def getSeqFromSeq(file: NodeSeq, s:String, s2: String): NodeSeq = {
+    val first = (file \\ s)
+    val second = (first \\ s2)
+    val end = (second \\ "card")
+    end
   }
 
   override def save(game: GameInterface): Unit = saveString(game)
